371f67ab28412042345c7203604e83f5fa79964b
lnferris <lnferris@alum.mit.edu>
2020-06-18 12:29:20 -0400

    ocean_data_tools_1.0.0

--- bathymetry_section.m
+++ bathymetry_section.m
@@ -1,7 +1,7 @@
 %  Author: Laur Ferris
 %  Email address: lnferris@alum.mit.edu
 %  Website: https://github.com/lnferris/ocean_data_tools
-%  Jun 2020; Last revision: 15-Jun-2020
+%  Jun 2020; Last revision: 18-Jun-2020
 %  Distributed under the terms of the MIT License
 
 % inputs: 
