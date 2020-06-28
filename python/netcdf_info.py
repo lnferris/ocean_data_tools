94ed0eccc95dff29b6591e01bde90357e872634c
Laur Ferris <lnferris@alum.mit.edu>
2020-06-28 12:56:03 -0400

    Update netcdf_info.py

--- python/netcdf_info.py
+++ python/netcdf_info.py
@@ -1,12 +1,4 @@
 
-#!/usr/bin/env python
-
-#  Author: Laur Ferris
-#  Email address: lnferris@alum.mit.edu
-#  Website: https://github.com/lnferris/ocean_data_tools
-#  Jun 2020; Last revision: 16-Jun-2020
-#  Distributed under the terms of the MIT License
-
 import netCDF4
 
 # Call the dataset constructor for one of the data files.
